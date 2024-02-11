d099250b438d59b8256be7323fd1a1f8eada2fb9
Rajviv-kumar <vivekshaw246@gmail.com>
2024-02-11 22:52:19 +0530

    react changes & cleaning

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,13 @@
-
+import Chai from "./chai"
 
 function App() {
   
 
   return (
     <>
-     <h1>Chai aur React with vite | VS |</h1>
+     <h1>Chai aur React with vite</h1>
+     <Chai/>
+     <p>Best React series</p>
     </>
   )
 }
